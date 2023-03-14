68a2faed38ce15239edd84e38584ecdc3f6e6602
DESKTOP-NQCQC97\seek <seekchange@daum.net>
2023-03-14 18:12:41 +0900

    실습9 ....

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,6 +1,16 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+int put_values_into_array(void* arr, int start, int count, int value)
+{
+    size_t i;
+    int* p = (int*)arr;
+    for (i = start; i < count + start; ++i) {
+        p[i] = value;
+    }
+    return count;
+}
+
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 #define MIN(x, y) ((x) > (y) ? (y) : (x))
@@ -12,15 +22,5 @@
 #define RANGE_DESC(curr, max, min) for ((curr) = (max); (curr) >= (min); --(curr))
 
 #define SET(ary, start, count, value) (put_values_into_array((ary), (start), (count), (value)))
-
-int put_values_into_array(void* arr, int start, int count, int value)
-{
-    size_t i;
-    int* p = (int*)arr;
-    for (i = start; i < count + start; ++i) {
-        p[i] = value;
-    }
-    return count;
-}
 
 #endif /* MACROS_H */
