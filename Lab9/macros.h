47a70f0513ab5733c42f6b44058c50d09ceb49a4
DESKTOP-NQCQC97\seek <seekchange@daum.net>
2023-03-14 10:45:01 +0900

    /lab 9 세번째

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,20 +1,20 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define MAX(x, y) (x > y ? x : y)
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
-#define MIN(x, y) (x > y ? y : x)
+#define MIN(x, y) ((x) > (y) ? (y) : (x))
 
-#define ABS(x) (x < 0 ? -1 * x : x)
+#define ABS(x) ((x) < (0) ? -1 * (x) : (x))
 
-#define RANGE(curr, min, max) for (curr = min; curr <= max; ++curr)
+#define RANGE(curr, min, max) for ((curr) = (min); (curr) <= (max); ++(curr))
 
-#define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; --curr)
+#define RANGE_DESC(curr, max, min) for ((curr) = (max); (curr) >= (min); --(curr))
 
 #define SET(ary, start, count, value) { \
                                                    int idx = 0; \
-                                                   while (idx != count) { \
-                                                       ary[(start) + idx++] = value; \
+                                                   while (idx != (count)) { \
+                                                       ary[(start) + idx++] = (value); \
                                                    } \
                                                } \
 
