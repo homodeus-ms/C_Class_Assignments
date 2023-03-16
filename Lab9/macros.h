a1d21fc5487dedd7fa9a4ec32ece26fa71e58912
DESKTOP-NQCQC97\seek <seekchange@daum.net>
2023-03-16 15:56:56 +0900

    실습9 아마도?

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -11,7 +11,15 @@
 
 #define RANGE_DESC(curr, max, min) for ((curr) = (max); (curr) >= (min); --(curr))
 
-#define SET(ary, start, count, value) (get_bool_from_count_make_arr((ary), (start), (count), (value)))
+#define SET(ary, start, count, value)                                                       \
+    ((count) > 0 ? (count) : 0);                                                               \
+    {                                                                                                \
+        size_t temp_idx;                                                                        \
+        for (temp_idx = (start); temp_idx < ((start) + (count)); ++temp_idx) {    \
+            ary[temp_idx] = (value);                                                          \
+        }                                                                                            \
+    } if (0)                                                                                         \
+
     
 int get_bool_from_count_make_arr(void* arr, int start, int count, int value);
 
