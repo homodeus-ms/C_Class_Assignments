abd088be40c5d988ca2080353a614a97614c6e9c
DESKTOP-NQCQC97\seek <seekchange@daum.net>
2023-03-27 22:58:16 +0900

    실습 11 두번째

--- Lab11/data_store.c
+++ Lab11/data_store.c
@@ -122,7 +122,7 @@ int update_password(user_t** users_or_null, unsigned int id, const char* passwor
     return TRUE;
 }
 
-void convert_str_to_release_mode(char* str, char mark)
+void convert_str_to_hide_mode(char* str, char mark)
 {
     char first_letter = *str;
     
